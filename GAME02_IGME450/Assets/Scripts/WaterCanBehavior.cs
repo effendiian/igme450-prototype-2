432958590558baae236582826aa155a4ed6fce45
Mitch Heard (RIT Student) <mwh2719@ad.rit.edu>
2020-02-22 12:29:28 -0500

    Starting water can challenge
    Setting up basic framework for watering can challenge

---
+++ GAME02_IGME450/Assets/Scripts/WaterCanBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCanBehavior : MonoBehaviour
+{
+    //variables
+    public GameObject waterCan; //GameObject to hold water can object that is set in editor
+    public GameObject dirt; //GameObject to be set in editor that will change the dirt to make it look more dry
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //mybe change this to simply check phone oreientation instead of rate
+
+        //checking to see if user rotates phone
+        if(Input.gyro.rotationRate.x > 5)
+        {
+            SolvingChallenge();
+        }
+    }
+
+    //function to set up the chaallenge
+    public void Setup()
+    {
+        //move the water can to be slightly visible on screen
+
+
+
+        //change the dirt to make it look dry
+
+
+    }
+
+    //function to be called when the user rotates phone to solve challenge
+    private void SolvingChallenge()
+    {
+        //add water can tipping and pouring for a few seconds
+
+
+        //add fixing dirt color
+
+
+        //remove watering can from screen
+    }
+}
