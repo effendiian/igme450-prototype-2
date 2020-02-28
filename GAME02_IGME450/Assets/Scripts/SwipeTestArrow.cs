241403c66cd9311ef9421ac6e2870a0b2ea41f3a
Steven Domitrz <stevendomitrz@gmail.com>
2020-02-27 20:17:14 -0500

    Created a way to get swipe inputs, with an arrow to display potential

---
+++ GAME02_IGME450/Assets/Scripts/SwipeTestArrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeTestArrow : SwipeObserver
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnSwipe(Vector2 swipe)
+    {
+        float rotation = Mathf.Atan2(swipe.y, swipe.x);
+        Debug.Log(rotation);
+
+        RectTransform rect = this.gameObject.GetComponent<RectTransform>();
+        rect.rotation = Quaternion.Euler(0, 0, rotation * Mathf.Rad2Deg);
+
+        float scale = 0.25f + (3.75f * (swipe.magnitude / 700f));
+        scale = Mathf.Min(scale, 4);
+        rect.localScale = new Vector2(scale, 1);
+    }
+}
