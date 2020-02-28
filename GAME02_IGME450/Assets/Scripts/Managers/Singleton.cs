b4e7fb5473a3970eb6370dd16fd2b7d5329b1d24
Ian Effendi <effendiian@gmail.com>
2020-02-28 03:18:42 -0500

    Add support for Singleton classes

---
+++ GAME02_IGME450/Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MHO.Extensions;
+
+/// <summary>
+/// Inherit from this base class to create a singleton.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Singleton<T> : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
