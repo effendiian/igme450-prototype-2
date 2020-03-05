30f51e0f12585fa144939b5242c048ab8ae468a3
Ian Effendi <effendiian@gmail.com>
2020-03-05 06:14:59 -0500

    Add background

---
+++ GAME02_IGME450/Assets/Scripts/Controllers/BackgroundController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+
+public class BackgroundController : MonoBehaviour
+{
+
+    /// <summary>
+    /// Image component used for the background.
+    /// </summary>
+    [SerializeField, Required]
+    private Image image; 
+
+    /// <summary>
+    /// Color tint for background.
+    /// </summary>
+    [SerializeField]
+    private Color tint;
+
+    /// <summary>
+    /// Validate application of tint.
+    /// </summary>
+    [ExecuteInEditMode]
+    public void OnValidate() => this.ApplyTint();
+
+    /// <summary>
+    /// Apply the tint if it's different than the current.
+    /// </summary>
+    public void ApplyTint()
+    {
+        if (this.image)
+        {
+            this.image.color = tint;
+        }
+    }
+
+    /// <summary>
+    /// Set the background.
+    /// </summary>
+    /// <param name="background">Background to set.</param>
+    /// <param name="tint">Tint to set.</param>
+    public void SetBackground(Sprite background, Color? tint = null)
+    {
+        if (this.image)
+        {
+            this.image.sprite = background ?? this.image.sprite;
+            this.tint = tint.HasValue ? tint.Value : this.tint;
+            this.ApplyTint();
+        }
+    }
+
+
+}
---
+++ GAME02_IGME450/Assets/Scripts/Controllers/DebugCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCameraController : MonoBehaviour
+{
+
+    public Camera _debugCamera;
+
+    /// <summary>
+    /// Remove debug camera if it exists.
+    /// </summary>
+    public void Awake()
+    {
+        if (_debugCamera)
+        {
+            Destroy(_debugCamera.gameObject);
+        }
+    }
+
+}
